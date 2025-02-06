463fc33945238d9b64121b697ef90f2f0ae64966
IsthmusAlien <131596732+IsthmusAlien@users.noreply.github.com>
2025-02-07 01:46:17 +0530

    Home Page Initial
    Need a Header with 2 buttons, employer and student

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,35 @@ import "./css/Home.css";
 
 function Home() {
   return (
+    
     <div className="Home">
+
+      <div className="bgimg-1">
+        <div className="caption">
+          <h2>Internsphere</h2>
+          <p>By Students For Students</p>
+        </div>
+      </div>
+
+      <div 
+        style={{ 
+          position: "relative"
+         }}
+        >
+
+        <div
+          style={{
+            color: "#ddd",
+            backgroundColor: "#282E34",
+            textAlign: "justify",
+            padding: "50px 80px"
+          }}
+        >
+
+          <p>Educational Quote Here.</p>
+
+        </div>
+      </div>
 
     </div>
   );
